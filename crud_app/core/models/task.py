395a8c459527975912456b5c5f8180e39265e286
kodunaiff <kodunaiff@gmail.com>
2025-08-23 00:47:09 +0800

    update task models

--- crud_app/core/models/task.py
+++ crud_app/core/models/task.py
@@ -1,14 +1,17 @@
-import uuid
-
-from sqlalchemy import Column, String, Enum
+from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.dialects.postgresql import UUID as PG_UUID
-
+from uuid import uuid4, UUID
+from sqlalchemy import String, Enum
 from core.enums import TaskStatus
 from .base import Base
 
 
 class Task(Base):
-    id = Column(PG_UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
-    title = Column(String, nullable=False)
-    description = Column(String, nullable=True)
-    status = Column(Enum(TaskStatus, name="task_status"), nullable=False, default=TaskStatus.CREATED)
+    id: Mapped[UUID] = mapped_column(PG_UUID(as_uuid=True), primary_key=True, default=uuid4)
+    title: Mapped[str] = mapped_column(String, nullable=False)
+    description: Mapped[str] = mapped_column(String, nullable=True)
+    status: Mapped[TaskStatus] = mapped_column(
+        Enum(TaskStatus, name="task_status"),
+        default=TaskStatus.CREATED,
+        nullable=False
+    )
